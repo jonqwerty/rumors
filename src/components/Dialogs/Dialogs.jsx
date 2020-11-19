68c11ec6c5e807a8743dfa18fb54feb63b24d3b2
Your Name <you@example.com>
2020-11-19 21:08:28 +0200

    created reducers

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/state';
+import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogs-reducer';
 import DialogItem from './DialogItem/DialogItem';
 import  s from './Dialogs.module.css'
 import Message from './Message/Message';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,14 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
+        
+            <div>
             <img src='https://webstockreview.net/images/clipart-ear-science-sound-1.png' />
+            </div>
+            <div className={s.item}>
+                <p>RUMORS</p>
+            </div>
+                   
            
         </header>
     );
