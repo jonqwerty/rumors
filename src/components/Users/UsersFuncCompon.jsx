afb5eccd5ce405b583b2532e15297884db1f898a
Your Name <you@example.com>
2020-11-22 17:19:56 +0200

    method compotentDidMount. App works

--- src/components/Users/UsersFuncCompon.jsx
+++ src/components/Users/UsersFuncCompon.jsx
@@ -4,26 +4,31 @@ import styles from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from './../../assets/images/default_image.png'
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+let Users = (props) => {
+
+    let getUsers = () =>
+    {
+        if (props.users.length === 0) {
+
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
                 .then(response => {
-                    this.props.setUsers(response.data.items)
-                });   
+                    props.setUsers(response.data.items)
+                });
 
+        }
     }
-
-    render () {
-        return <div>
+    
+    return <div>
+        <button onClick={getUsers}>Get Users</button>
         {
-            this.props.users.map( u => <div key={u.id}>
+            props.users.map( u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={ u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         { u.followed 
-                        ? <button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button> : <button onClick={() => {this.props.follow(u.id)}}>Follow</button> }
+                        ? <button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> : <button onClick={() => {props.follow(u.id)}}>Follow</button> }
                         
                     </div>
                 </span>
@@ -40,8 +45,6 @@ class Users extends React.Component {
             </div>)
         }
     </div>
-
-    }
 }
-        
+
 export default Users;
