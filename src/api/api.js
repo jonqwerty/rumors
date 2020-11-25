8fe440cafbfe446b4919a8f4c95006687f2776ec
Your Name <you@example.com>
2020-11-26 00:09:18 +0200

    implemented DAL for comunication with API

---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+import * as axios from 'axios';
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "api-key": "59e559f3-c48c-45df-865e-82e51afdf429"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage=1, pageSize=10)  {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+        .then(response => {
+            return response.data;
+        });
+    
+    }  
+}
+
+
