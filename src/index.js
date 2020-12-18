c0f7ae3e921494f38068a72be8a661bdb681c5f5
Your Name <you@example.com>
2020-12-18 22:35:23 +0200

    changed hashrouter

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter >
     <React.StrictMode>
       <Provider store={store}>
         <App  />
       </Provider>
     </React.StrictMode>
-    </BrowserRouter>,    document.getElementById('root'));
+    </HashRouter>,    document.getElementById('root'));
 
 
 
